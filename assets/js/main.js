b8a9daba8710a1fa69ac0704b4963e9332ea14c2
Willem Nelissen <32498171+willemnelissen@users.noreply.github.com>
2020-04-13 15:07:00 +0200

    foto's: fix default filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -120,7 +120,8 @@
   $(window).on('load', function() {
     var portfolioIsotope = $('.portfolio-container').isotope({
       itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
+      layoutMode: 'fitRows',
+      filter: '.filter-2019'
     });
 
     $('#portfolio-flters li').on('click', function() {
