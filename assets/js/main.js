3b8a5cd9b95db7abc44fa682448a65fb04af26e8
Willem Nelissen <32498171+willemnelissen@users.noreply.github.com>
2020-05-08 10:58:47 +0200

    confetti?

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@
       animation.onfinish = () => {particle.remove();};
     }, 5);
   }
-  confetti(150,150)
+
   // Slider bij vrienden
   $(document).ready(function() {
     const $rewardBox = $('#vrienden-rewards')
@@ -91,7 +91,7 @@
         $rewardBox.addClass("vriend-level-5")
         var horizontalCenter = Math.floor(window.innerWidth/2);
         var verticalCenter = Math.floor(window.innerHeight/2);
-      //  confetti(horizontalCenter,verticalCenter)
+        confetti(horizontalCenter,verticalCenter)
       }
     });
   });
